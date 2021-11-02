6fe11278f410df87b1e2e91748aaeb03daf9466e
ronskie69 <90790271+ronskie69@users.noreply.github.com>
2021-11-02 12:45:27 +0800

    Delete index.js

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("secret_id").value = Math.round(Math.random() * 999999);
-const room_id = document.getElementById("room_id");
-const generate = document.getElementById("generate"); 
-generate.addEventListener('click', e => {
-    e.preventDefault()
-    let random = Math.round(Math.random() * 9999);
-    room_id.value = random;
-});
